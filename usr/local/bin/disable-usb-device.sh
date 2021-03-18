eca27c887d35af12718d6ad52bdf7186566005fb
typoworx-de <info@typoworx.com>
2021-03-18 11:20:19 +0100

    Create disable-usb-device.sh

---
+++ usr/local/bin/disable-usb-device.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+getdevice()
+ {
+    idV=${1%:*}
+    idP=${1#*:}
+    for path in `LC_ALL=C find /sys/ -name idVendor 2>&1 | grep -v 'Permission denied' | rev | cut -d/ -f 2- | rev`;
+    do
+        if grep -q $idV $path/idVendor; then
+            if grep -q $idP $path/idProduct; then
+                LC_ALL=C find $path -name 'authorized' -exec sh -c 'echo 0 | sudo tee {} > /dev/null' \; -quit 2>&1 | grep -v 'Permission denied';
+
+                [[ $(cat /sys/devices/pci0000:00/0000:00:14.0/usb1/1-6/1-6.2/1-6.2:1.0/authorized) == 0 ]] && {
+                    return 0;
+                }
+            fi
+        fi
+    done
+
+    return 1;
+}
+
+getdevice "${1}" && {
+  echo 'Success';
+  exit 0;
+}
+
+exit 1;
