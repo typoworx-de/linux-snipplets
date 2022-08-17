55e332fb14e1e85119b09f1c644827f2fd5d49d8
GKN <info@typoworx.com>
2022-08-17 19:31:56 +0200

    Update, fixing some stuff

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+_pwd=$(realpath $(dirname $0))
+
+[ "$EUID" != 0 ] && {
+  echo "This script must be run as root!";
+  exit 1;
+}
+
+[ -L "/etc/profile.d/scratch.sh" ] || {
+  ln -s "${_pwd}/scratch.rc" "/etc/profile.d/scratch.sh";
+}
+
+[ -L "/etc/sudoers.d/scratch-sudoers" ] || {
+  chown root:$USER "${_pwd}/etc/sudoers.d/scratch-sudoers";
+  ln -s "${_pwd}/etc/sudoers.d/scratch-sudoers" "/etc/sudoers.d/scratch-sudoers";
+}
+
