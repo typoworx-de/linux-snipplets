59fd63024b9c1f176a142cc50312338eb2192278
GKN <info@typoworx.com>
2023-12-06 11:02:27 +0100

    Update

--- etc/aliases.d/docker@scratch.sh
+++ etc/aliases.d/docker@scratch.sh
@@ -4,25 +4,25 @@ alias docker-build="docker-timed buildx build --progress=plain --cache-to=type=i
 alias docker-lint="docker run --rm -i hadolint/hadolint <"
 
 # Docker-Composer Generic Shorthands
-alias docker-compose="docker compose"
+alias docker-compose="docker compose --compatibility"
 alias docker-compose-up="docker-compose-stack-up"
 alias docker-compose-down="docker-compose-stack-down"
 alias docker-compose-run="docker-compose-service-run"
 
 # Build Commands
-#alias docker-compose-build="docker-compose build --push --build-arg BUILD_TIME=$(date +%s)"
-alias docker-compose-build="docker-compose build --build-arg BUILD_TIME=$(date +%s)"
-alias docker-compose-build-up="docker-compose up --build --push --include-deps --force-recreate --always-recreate-deps --remove-orphans --wait"
+#alias docker-compose-build="docker compose build --push --build-arg BUILD_TIME=$(date +%s)"
+alias docker-compose-build="docker compose build --build-arg BUILD_TIME=$(date +%s)"
+alias docker-compose-build-up="docker compose up --build --push --include-deps --force-recreate --always-recreate-deps --remove-orphans --wait"
 
 # Stack Commands
-alias docker-compose-stack-up="docker-compose up --always-recreate-deps --remove-orphans --wait"
-alias docker-compose-stack-restart="docker-compose down; docker-compose-up"
-alias docker-compose-stack-down="docker-compose down --remove-orphans -v"
+alias docker-compose-stack-up="docker compose up --always-recreate-deps --remove-orphans --wait"
+alias docker-compose-stack-restart="docker compose down; docker-compose-up"
+alias docker-compose-stack-down="docker compose down --remove-orphans -v"
 
 # Service Commands
-alias docker-compose-service-run="docker-compose run --rm"
-alias docker-compose-service-rm="docker-compose rm --stop"
-#alias docker-compose-service-restart="docker-compose rm --stop -f $@; docker-compose up -d $@"
+alias docker-compose-service-run="docker compose run --rm"
+alias docker-compose-service-rm="docker compose rm --stop"
+#alias docker-compose-service-restart="docker compose rm --stop -f $@; docker-compose up -d $@"
 
 # Misc Docker Tools
 alias docker-dive="docker run -ti --rm  -v /var/run/docker.sock:/var/run/docker.sock wagoodman/dive"
@@ -34,8 +34,8 @@ function removeAlias()
 }
 
 removeAlias unalias docker-compose-service-restart
-function docker-compose-service-restart()
+function docker-compose-service-restart
 {
-  docker-compose rm --stop -f $@;
-  docker-compose up -d $@;
+  docker compose rm --stop -f $@;
+  docker compose up -d $@;
 }
--- scratch.lib.sh
+++ scratch.lib.sh
@@ -1,7 +1,1 @@
 #!/bin/bash
-
-function scratchGit()
-{
-  gitRoot=$(realpath ${scratchGitRoot});
-  git --git-dir=${gitRoot}/.git --work-tree=${gitRoot} $@
-}
